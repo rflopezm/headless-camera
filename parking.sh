783a31b618feda843427394bbaefcc032cc1fc38
Rodrigo López <rflopezm@gmail.com>
2021-08-09 20:40:27 -0400

    Launch scripts and drivers

---
+++ parking.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export BARRIER_SERVER="http://172.16.15.100:3000"
+export IOT_USERNAME="172.16.15.51"
+export IOT_PASSWORD="pw4X123EXIT"
+export BARRIER_STATE="IDLE_EXIT"
+export NODE_ENV="production"
+/home/pi/Parking.AppImage
